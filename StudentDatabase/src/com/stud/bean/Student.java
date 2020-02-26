e1bdd26a64057d31a080690fafbda53551887ab6
inu <dasswagatika004@gmail.com>
2020-02-26 02:53:24 -0800

    swagatika2nd commit

--- StudentDatabase/src/com/stud/bean/Student.java
+++ StudentDatabase/src/com/stud/bean/Student.java
@@ -2,124 +2,117 @@ package com.stud.bean;
 
 public class Student {
 	//instance variable
-	int id;
-	String name;
-	int english;
-	int science;
-	int social;
-	int maths;
-	int malayalum;
-	
+   private	int id;
+   private	String name;
+   private	int english;
+   private	int science;
+   private	int social;
+   private	int maths;
+   private	int malayalam;
+ private	int total;
+//default constructor
+   public Student(){
+	   
+   }
 
-	// default constructor
-	public Student() {
+public Student(int id, String name, int english, int science, int social,
+		int maths, int malayalam, int total) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.english = english;
+	this.science = science;
+	this.social = social;
+	this.maths = maths;
+	this.malayalam = malayalam;
+	this.total = total;
+}
+//parameteraised constructor
 
-	}
-	
-	public Student(String name, int english, int science, int social, int maths,
-			int malayalum) {
-		super();
-		this.name = name;
-		this.english = english;
-		this.science = science;
-		this.social = social;
-		this.maths = maths;
-		this.malayalum = malayalum;
-	}
+public Student(String name, int english, int science, int social, int maths,
+		int malayalam, int total) {
+	super();
+	this.name = name;
+	this.english = english;
+	this.science = science;
+	this.social = social;
+	this.maths = maths;
+	this.malayalam = malayalam;
+	this.total = total;
+}
+//getter n setter
 
-	//parameteraised constructor
+public int getId() {
+	return id;
+}
 
+public void setId(int id) {
+	this.id = id;
+}
 
-	public Student(int id, String name, int english, int science, int social,
-			int maths, int malayalum) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.english = english;
-		this.science = science;
-		this.social = social;
-		this.maths = maths;
-		this.malayalum = malayalum;
-	}
+public String getName() {
+	return name;
+}
 
-    //using getter and setter
-	public int getId() {
-		return id;
-	}
+public void setName(String name) {
+	this.name = name;
+}
 
+public int getEnglish() {
+	return english;
+}
 
-	public void setId(int id) {
-		this.id = id;
-	}
+public void setEnglish(int english) {
+	this.english = english;
+}
 
+public int getScience() {
+	return science;
+}
 
-	public String getName() {
-		return name;
-	}
+public void setScience(int science) {
+	this.science = science;
+}
 
+public int getSocial() {
+	return social;
+}
 
-	public void setName(String name) {
-		this.name = name;
-	}
+public void setSocial(int social) {
+	this.social = social;
+}
 
+public int getMaths() {
+	return maths;
+}
 
-	public int getEnglish() {
-		return english;
-	}
+public void setMaths(int maths) {
+	this.maths = maths;
+}
 
+public int getMalayalam() {
+	return malayalam;
+}
 
-	public void setEnglish(int english) {
-		this.english = english;
-	}
+public void setMalayalam(int malayalam) {
+	this.malayalam = malayalam;
+}
 
+public int getTotal() {
+	return total;
+}
 
-	public int getScience() {
-		return science;
-	}
-
-
-	public void setScience(int science) {
-		this.science = science;
-	}
-
-
-	public int getSocial() {
-		return social;
-	}
-
-
-	public void setSocial(int social) {
-		this.social = social;
-	}
-
-
-	public int getMaths() {
-		return maths;
-	}
-
-
-	public void setMaths(int maths) {
-		this.maths = maths;
-	}
-
-
-	public int getMalayalum() {
-		return malayalum;
-	}
-
-
-	public void setMalayalum(int malayalum) {
-		this.malayalum = malayalum;
-	}
-	//generate to string
-
-
-	@Override
-	public String toString() {
-		return "Student [id=" + id + ", name=" + name + ", english=" + english
-				+ ", science=" + science + ", hindi=" + social + ", maths="
-				+ maths + ", malayalum=" + malayalum + "]";
-	}
-	
+public void setTotal(int total) {
+	this.total = total;
+}
+//to string
+@Override
+public String toString() {
+	return "Student [id=" + id + ", name=" + name + ", english=" + english
+			+ ", science=" + science + ", social=" + social + ", maths="
+			+ maths + ", malayalam=" + malayalam + ", total=" + total + "]";
+}
+   
+   
 }
 	
