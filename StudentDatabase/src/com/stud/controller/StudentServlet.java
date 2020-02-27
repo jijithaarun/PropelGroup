51c5943602a86cf50e2574ff178ea152e555def0
nimex <nimexmail@gmail.com>
2020-02-26 16:26:12 -0800

    nimex second commit

--- StudentDatabase/src/com/stud/controller/StudentServlet.java
+++ StudentDatabase/src/com/stud/controller/StudentServlet.java
@@ -1,11 +1,15 @@
 package com.stud.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.stud.bean.Student;
 
 @WebServlet("/StudentServlet")
 public class StudentServlet extends HttpServlet {
@@ -32,23 +36,39 @@ public class StudentServlet extends HttpServlet {
 	private void viewStudentMark(HttpServletRequest request,
 			HttpServletResponse response) {
 
+		// creating object for StudentDao
+		StudentDao studentDao = new StudentDao();
+		List<Student> listStudent = studentDao.display();
+
+		// setting value into List
+		request.setAttribute("listStudent", listStudent);
+
+		request.getRequestDispatcher("/JSP/Student.jsp").forward(request,
+				response);
 	}
 
 	private void inserStudentMark(HttpServletRequest request,
 			HttpServletResponse response) throws IOException {
 
-		// readubg
+		// reading values
 		String name = request.getParameter("name");
 		Integer maths = Integer.parseInt(request.getParameter("maths"));
 		Integer malayalam = Integer.parseInt(request.getParameter("malayalam"));
 		Integer english = Integer.parseInt(request.getParameter("english"));
 		Integer science = Integer.parseInt(request.getParameter("science"));
 		Integer social = Integer.parseInt(request.getParameter("social"));
+
+		// calculating total
 		Integer total = maths + malayalam + english + science + social;
 
 		// creating object Student
-
+		Student student = new Student(name, english, science, social, maths,
+				malayalam, total);
 		// creating object for StudentDao
+		SudentDao studentDao = new StudentDao();
+
+		// calling insert method
+		studentDao.insert(student);
 
 		response.sendRedirect("StudentServlet?action=view");
 
